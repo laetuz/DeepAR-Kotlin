a392b8a6e0b00d9b310fbd75a16a223fea3ea698
Ryo Martin <laetuzg@gmail.com>
2023-05-13 00:06:18 +0700

    Converted 3 more Java class to Kotlin.

--- app/src/main/java/com/neotica/deeparkotlin/BasicActivity.kt
+++ app/src/main/java/com/neotica/deeparkotlin/BasicActivity.kt
@@ -1,17 +1,14 @@
-package com.neotica.deeparkotlin;
+package com.neotica.deeparkotlin
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-public class BasicActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basic);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+class BasicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
